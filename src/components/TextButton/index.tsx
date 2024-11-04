fcaec0a722998ddee8070898664546c9e6b85e2e
Charlie131 <charlieeelages@gmail.com>
2024-11-03 23:24:06 -0300

    refactor: text button types added

--- src/components/TextButton/index.tsx
+++ src/components/TextButton/index.tsx
@@ -1,16 +1,23 @@
 import './styles.css';
 
+export enum BtnTypes {
+    BUTTON = "button",
+    SUBMIT = "submit",
+    RESET = "reset"
+}
+
 interface TextButtonProps {
     className?: string;
+    type?: BtnTypes;
     text: string;
-    onClick: () => void;
+    onClick?: () => void;
     textSize?: number;
 }
 
 export default function TextButton(props: TextButtonProps) {
 
     return (
-        <button onClick={props.onClick} className={`txt-btn-wrapper${props.className ? ` ${props.className}` : ""}`}>
+        <button type={props.type || BtnTypes.BUTTON} onClick={props.onClick} className={`txt-btn-wrapper${props.className ? ` ${props.className}` : ""}`}>
             <h4  className='text'>{props.text}</h4>
         </button>
     );
