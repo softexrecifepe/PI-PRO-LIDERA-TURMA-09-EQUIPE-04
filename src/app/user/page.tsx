e00d5fea0cc5e7dfab1a22aff42e38a043e51b57
Charlie131 <charlieeelages@gmail.com>
2024-11-21 21:59:32 -0300

    feat: implemented user page functionality

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,8 +1,42 @@
 "use client";
 import { useRouter } from "next/navigation";
 
+import { useUser } from "@/contexts/UserContext";
+import IconTab, { VAlignOptions } from "@/components/IconTab";
+import Header from "@/components/Header"
+import TextButton from "@/components/TextButton"
+import InputField from "@/components/InputField" 
+import { logoutUser } from "@/services/userService";
 
 export default function User() {
     const router = useRouter();
-    router.push("/user/login")
+    const { user, loading } = useUser();
+
+    if (loading) {
+        return <p>Carregando...</p>;
+    }
+
+    if (!user) {
+        router.push("/user/login")
+
+    } else {
+        return (
+            <div>
+                <Header>
+                    <IconTab route="/help" iconVAlingn={VAlignOptions.MIDDLE} name="Ajuda" icon="bi bi-question"/>
+                    <IconTab route="/user" iconVAlingn={VAlignOptions.BOTTOM} name={user ? user.username : "Entrar"} icon="bi bi-person-fill"/>
+                </Header>
+                <div>
+                    <h1>Bem-vindo, {user?.name}!</h1>
+                    <p>Email: {user?.email}</p>
+                    <TextButton text="Sair" onClick={logoutUser}/>
+                </div>
+            </div>
+        );
+    }
+    
+    
 }
+
+
+  
